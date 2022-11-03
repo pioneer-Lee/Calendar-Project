61c8adbe0d509b5b9e8f5e932cd3518f0ac0ea3a
ihyeong-in <ihyeong-in@localhost>
2022-11-04 00:08:56 +0900

    Scanner 활용 두 수의 합 구하

--- src/Lee/Calendar/Calendar.java
+++ src/Lee/Calendar/Calendar.java
@@ -1,9 +1,20 @@
 package Lee.Calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 
 	public static void main(String[] args) {
-		System.out.println(2);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("두 수를 입력하세요");
+		String InputValue = sc.nextLine();
+		sc.close();
+		String[] SplitValue = InputValue.split(" ");
+		int first = Integer.parseInt(SplitValue[0]);
+		int second = Integer.parseInt(SplitValue[1]);
+		int sum = first + second;
+		System.out.println("두 수의 합은 " + sum + "입니다.");
+
 	}
 
 }

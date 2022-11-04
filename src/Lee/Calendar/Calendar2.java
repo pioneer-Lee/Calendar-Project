4d8e9f7c89554c4cbe5bcba0b6b520cfaf5d180a
ihyeong-in <ihyeong-in@localhost>
2022-11-04 21:35:34 +0900

    종료 조건 포함

--- src/Lee/Calendar/Calendar2.java
+++ src/Lee/Calendar/Calendar2.java
@@ -15,15 +15,27 @@ public class Calendar2 {
 		Scanner scanner = new Scanner(System.in);
 		Calendar2 c2 = new Calendar2();
 
-		System.out.println("반복횟수를 입력하세요");
-		int inputRepeat = scanner.nextInt();
 		System.out.println("월을 입력하세요");
-		for(int i = 1; i <= inputRepeat; i++) {
+
+		while (true) {
 			int inputMonth = scanner.nextInt();
-			System.out.println(c2.getMaxDaysOfMonth(inputMonth)); 
+
+			if (1 <= inputMonth && inputMonth <= 12) {
+				if (c2.getMaxDaysOfMonth(inputMonth) == 31) {
+					System.out.println(31);
+				} else if (c2.getMaxDaysOfMonth(inputMonth) == 30) {
+					System.out.println(30);
+				} else {
+					System.out.println("4n+2일 경우 29, 그 외 28");
+				}
+			} else if (inputMonth == -1) {
+				System.out.println("have a nice day~");
+				break;
+			} else {
+				System.out.println("1 ~ 12만 입력 가능합니다 종료하고 싶으면 -1을 입력하세요");
+			}
 		}
+
 		scanner.close();
 	}
 }
-		
-

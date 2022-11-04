826f7586c7b1335171af03fb17cede24ab8c21c8
ihyeong-in <ihyeong-in@localhost>
2022-11-04 21:01:52 +0900

    반복 입력 가능 기능 추

--- src/Lee/Calendar/Calendar2.java
+++ src/Lee/Calendar/Calendar2.java
@@ -1,31 +1,29 @@
 package Lee.Calendar;
+
 import java.util.Scanner;
+
 public class Calendar2 {
+
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	public int getMaxDaysOfMonth(int inputMonth) {
+		return MAX_DAYS[inputMonth - 1];
+	}
 
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
-		
-		System.out.println("달을 입력하세요");
-		int inputMonth = scanner.nextInt();
-		
-		int[] month = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-		if(1 <= inputMonth && inputMonth <= 12) {
-			if(month[inputMonth-1] == 31) {
-				System.out.println(31);
-			}
-			else if(month[inputMonth-1] == 30) {
-				System.out.println(30);
-			}
-			else {
-				System.out.println("if year = 4n + 2, 29 else 28");
-			}
+		Calendar2 c2 = new Calendar2();
+
+		System.out.println("반복횟수를 입력하세요");
+		int inputRepeat = scanner.nextInt();
+		System.out.println("월을 입력하세요");
+		for(int i = 1; i <= inputRepeat; i++) {
+			int inputMonth = scanner.nextInt();
+			System.out.println(c2.getMaxDaysOfMonth(inputMonth)); 
 		}
-			else {
-				System.out.println(" 1에서 12사이만 입력하세요"); 
-			}
-		
 		scanner.close();
 	}
-
 }
+		
+
